1a9551ab13310a79817b213b803f24544527081f
Michael Eclavea <usermike671@gmail.com>
2020-12-18 22:15:59 -0800

    added coin toss

--- gametest/src/components/Luck.js
+++ gametest/src/components/Luck.js
@@ -1,25 +1,48 @@
-import React from 'react'
 import client from './connect.js';
+import React, {useState} from 'react'
+import './Luck.css';
+import './Buttons.css';
 
-const Luck = (props) => {
+
+function Luck(props) {
   let luckTag = '';
-  const flipCoin = () => {
-    let luck = (Math.floor(Math.random() * Math.floor(2)));
-    if (luck === 0) {
-      luckTag = 'You had poor luck';
-    } else {
-      luckTag = 'You had good luck';
+  const [tails, setTails] = useState('tails')
+  const [heads, setHeads] = useState('heads')
+
+  
+  function tossCoin(){  
+    let count = Math.floor(Math.random() * 5) + 1; 
+    if(count == 1){
+        setHeads('animate-tails');
+        setTails('animate-heads');
+        let luck = 0;
+        luckTag = 'You had poor luck';
+        let payload = { luck , scenario: props.scenario }
+        client.emit('luck', payload);
     }
-    let payload = { luck , scenario: props.scenario }
-    client.emit('luck', payload);
+    else{
+        setHeads('animate-heads');
+        setTails('animate-tails');
+        let luck = 1; 
+        luckTag = 'You had good luck';
+        let payload = { luck , scenario: props.scenario }
+        client.emit('luck', payload);
+      }
   }
 
   return (
     <div>
-      <button onClick={flipCoin} id="luck"> Flip Coin </button>
-      <p>{luckTag}</p>
+      <div className="container">
+        <p>{luckTag}</p>
+      </div>
+      <div id="coin">
+        <div className={heads} style={{backgroundImage: 'url(./images/heads.png)', height: '202px', width: '198px',}}/>
+        <div className={tails} style={{backgroundImage: 'url(./images/tails.png)', height: '202px', width: '198px',}}/>
+      </div>
+        <button onClick={tossCoin} className='choiceButtons'>Toss coin</button>
     </div>
   )
 }
 
-export default Luck
+export default Luck;
+

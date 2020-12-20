ab636cb4bb5a7cd7454988258637fb990a8db1dc
Kevin McNeil <dogwoodrawks@gmail.com>
2020-12-20 10:28:37 -0800

    heroku test

--- gametest/src/components/connect.js
+++ gametest/src/components/connect.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const client = io.connect('https://codequest-fight-forthe-kingdom.herokuapp.com/', { transports: ['websocket'] });
+const client = io.connect('http://localhost:3001', { transports: ['websocket'] });
 client.on('connect', () => {
   console.log('connected');
 });

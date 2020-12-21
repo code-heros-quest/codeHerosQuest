d2fd12cc3cf982b6cab9f122b64f637021dd5152
Kevin McNeil <dogwoodrawks@gmail.com>
2020-12-21 12:02:06 -0800

    fixed questions and video stuff

--- gametest/src/components/Choice2.js
+++ gametest/src/components/Choice2.js
@@ -12,7 +12,7 @@ const Choice2 = (props) => {
 
   const makeChoice = (e) => {
     let vote = e.target.id;
-    let payload = { vote , scenario: props.scenario }
+    let payload = { vote, scenario: props.scenario }
     client.emit('choice', payload);
     setStyle('disabled')
     setShow('false')
@@ -22,6 +22,9 @@ const Choice2 = (props) => {
 
   return (
     <div>
+      <div style={{ padding: '10px 5px 5px 5px', borderRadius: '8px', border: '9px solid #854215', width: '50%', height: 'auto', margin: 'auto', backgroundImage: 'url(./images/textBox/text1.png)', backgroundSize: 'cover', backgroundAttachment: 'fixed' }}>
+        <h5 style={{ color: 'black' }}>{props.scenario.choiceQuestion}</h5>
+      </div>
       <button className={style} id="1" onClick={makeChoice} disabled={show}>{choice1}</button>
       <button className={style} id="2" onClick={makeChoice} disabled={show}>{choice2}</button>
     </div>

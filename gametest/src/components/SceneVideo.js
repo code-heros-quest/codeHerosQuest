b4979df0cf5cdb4b02c82917778f13e68996e802
MichaelEclavea <>
2021-02-09 15:55:44 -0800

    changed vid to iframe

--- gametest/src/components/SceneVideo.js
+++ gametest/src/components/SceneVideo.js
@@ -16,7 +16,7 @@ const SceneVideo = (props) => {
     if (props.scenario.number === 1) {
       return scene.map(({ video }, index) => (
         // <iframe autoPlay src={video} key={index} style={{ width: '100%', minwidth: '700px',  minHeight: '525px', height: '100%', border: 'none' }} autoPlay></iframe>
-        <video src={video} autoplay ></video>
+        <iframe src={video} autoplay ></iframe>
       ))
     } else {
       return scene.map(({ video }, index) => (
